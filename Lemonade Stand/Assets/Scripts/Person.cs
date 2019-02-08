de543f6ecc5cc34c51dd90b215f94e3144fcad19
Ryan Higgins <c16381886@mydit.ie>
2019-02-08 15:50:51 +0000

    City planning started, system for serving customers and money started

---
+++ Lemonade Stand/Assets/Scripts/Person.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour
+{
+    public GameObject serveIcon;
+
+    public bool isServed = false;
+    public  bool atStand = false;
+    private bool beenServed = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        serveIcon = gameObject.transform.Find("Serve Me Icon").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0,1*Time.deltaTime,0);
+        }
+        
+        if (isServed && atStand)
+        {
+            if (!beenServed)
+            {
+                LemonadeSystem.money += 1;
+                LemonadeSystem.customers += 1;
+                isServed = false;
+                beenServed = true;
+            }
+        } else if (!isServed && atStand)
+        {
+            serveIcon.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Lemonade Stand/Assets/Scripts/Serving.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Serving : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D customer)
+    {
+        if (customer.CompareTag("Customer"))
+        {
+            print("entered");
+            customer.gameObject.GetComponent<Person>().atStand = true;
+        }
+    }
+}
