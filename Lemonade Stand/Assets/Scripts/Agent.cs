94a787824d6864f7cfbacc75a000c9ef903f4e56
Eoin Ahern <eahern123@gmail.com>
2019-02-08 14:41:14 +0000

    Edited gizmos

---
+++ Lemonade Stand/Assets/Scripts/Agent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    public Vector3 targetPos;
+    public Vector3 nextWaypoint;
+
+
+}
